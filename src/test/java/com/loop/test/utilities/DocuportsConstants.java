38e512c5720fc4b02c502de26187e69d44926a04
manafovaytaj <manafova.aytac@gmail.com>
2024-01-03 20:53:14 -0600

    Commit

---
+++ src/test/java/com/loop/test/utilities/DocuportsConstants.java
@@ -0,0 +1,11 @@
+package com.loop.test.utilities;
+
+public class DocuportsConstants {
+
+    public static final  String USERNAME_CLIENT="b1g1_client@gmail.com";
+
+    public  static  final String  ERROR_MESSAGE_FOR_EMPTY_PASSWORD="Pleaase enter your password";
+
+    public  static  final  String LOGO_DOCUPORT="Docuport";
+
+}
