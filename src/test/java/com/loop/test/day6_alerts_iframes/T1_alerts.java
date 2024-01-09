c6cf5d217c057cbdacb7f734c2ee8e928e467a78
manafovaytaj <manafova.aytac@gmail.com>
2024-01-08 21:11:08 -0600

    Commit

---
+++ src/test/java/com/loop/test/day6_alerts_iframes/T1_alerts.java
@@ -0,0 +1,12 @@
+package com.loop.test.day6_alerts_iframes;
+
+import com.loop.test.base.TestBase;
+import org.testng.annotations.Test;
+
+public class T1_alerts extends TestBase {
+
+    @Test
+    public void informationAlert(){
+
+    }
+}
