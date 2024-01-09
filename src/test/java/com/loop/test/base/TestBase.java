25b53f1c36a060a7f3a4fbf7758d276980240a8e
manafovaytaj <manafova.aytac@gmail.com>
2024-01-08 23:38:19 -0600

    Commit

--- src/test/java/com/loop/test/base/TestBase.java
+++ src/test/java/com/loop/test/base/TestBase.java
@@ -15,9 +15,9 @@ public class TestBase {
         driver.manage().window().maximize();
     }
 
-    @AfterMethod
-    public void tearDownMethod(){
-        driver.close();
-    }
+//    @AfterMethod
+//    public void tearDownMethod(){
+//        driver.close();
+//    }
 
 }
