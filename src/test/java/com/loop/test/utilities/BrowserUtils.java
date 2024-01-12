b45ef93cbf254585e6d3035f2a1ae0e61730064a
manafovaytaj <manafova.aytac@gmail.com>
2024-01-11 19:47:23 -0600

    Commit

---
+++ src/test/java/com/loop/test/utilities/BrowserUtils.java
@@ -0,0 +1,30 @@
+package com.loop.test.utilities;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class BrowserUtils {
+    /**
+     * validate if driver switched to expected url and title
+     * @param driver
+     * @param expectedUrl
+     * @param expectedTitle
+     * @author nadir
+     * implements assertion
+     */
+    public static void switchWindowAndValidate(WebDriver driver, String expectedUrl, String expectedTitle){
+        expectedTitle = expectedTitle.toLowerCase();
+        expectedUrl = expectedUrl.toLowerCase();
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String each : windowHandles){
+            driver.switchTo().window(each);
+            if(driver.getCurrentUrl().toLowerCase().contains(expectedUrl)){
+                break;
+            }
+        }
+        assertTrue(driver.getTitle().toLowerCase().contains(expectedTitle));
+    }
+}
